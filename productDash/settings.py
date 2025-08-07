ac015f3842812a8b226772f7caf197355e9fad54
HAKIZIMANA Celestin <91734054+celhakiza@users.noreply.github.com>
2025-08-07 21:21:58 +0200

    deleted some files

--- productDash/settings.py
+++ productDash/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='unsafe-dev-key')
-
+SECRET_KEY = 'django-insecure-8wf8haa5so9algekhykwr^@8087mvpczb)y25ho^5h7750^0o)'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
